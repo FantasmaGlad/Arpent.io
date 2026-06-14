b75a9691e72b428f83ab968d8833b2c8d10beaac
fanta <fantacasino3012@gmail.com>
2026-06-15 00:12:50 +0200

    build: bump version to 1.2 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.fanta.androidsport"
         minSdk = 35
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
